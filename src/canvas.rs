5b30828204180cfc33af2c5e40d7ade2c9341512
meloalright <meloalright@gmail.com>
2022-04-30 12:34:42 +0800

    Remove some dead code

--- src/canvas.rs
+++ src/canvas.rs
@@ -24,22 +24,6 @@ impl Canvas {
         &mut self.paint
     }
 
-
-    #[inline]
-    pub fn save(&mut self) {
-        self.canvas().save();
-    }
-
-    #[inline]
-    pub fn translate(&mut self, dx: f32, dy: f32) {
-        self.canvas().translate((dx, dy));
-    }
-
-    #[inline]
-    pub fn scale(&mut self, sx: f32, sy: f32) {
-        self.canvas().scale((sx, sy));
-    }
-
     #[inline]
     pub fn move_to(&mut self, x: f32, y: f32) {
         self.begin_path();
@@ -49,11 +33,6 @@ impl Canvas {
     #[inline]
     pub fn line_to(&mut self, x: f32, y: f32) {
         self.path.line_to((x, y));
-    }
-
-    #[inline]
-    pub fn quad_to(&mut self, cpx: f32, cpy: f32, x: f32, y: f32) {
-        self.path.quad_to((cpx, cpy), (x, y));
     }
 
     #[allow(dead_code)]
@@ -82,24 +61,9 @@ impl Canvas {
     }
 
     #[inline]
-    pub fn fill(&mut self) {
-        self.paint.set_style(PaintStyle::Fill);
-        self.surface.canvas().draw_path(&self.path, &self.paint);
-    }
-
-    #[inline]
-    pub fn set_line_width(&mut self, width: f32) {
-        self.paint.set_stroke_width(width);
-    }
-
-    #[inline]
     pub fn data(&mut self) -> Data {
         let image = self.surface.image_snapshot();
         image.encode_to_data(EncodedImageFormat::PNG).unwrap()
     }
 
-    #[inline]
-    fn canvas(&mut self) -> &mut skia_safe::Canvas {
-        self.surface.canvas()
-    }
 }
